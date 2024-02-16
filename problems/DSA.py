e409a114293365814c52f87c763869b4308f3dff
charandas6300 <charandas.v20@iiits.in>
2024-02-16 16:23:48 +0530

    word search

--- problems/DSA.py
+++ problems/DSA.py
@@ -521,4 +521,28 @@
 #         for j in res:
 #             ans.append(list(j))
 #         return ans            
-        
+#####################################################wordsearch
+# from typing import List
+
+# class Solution:
+#     def exist(self, board: List[List[str]], word: str) -> bool:
+#         def dfs(i, j, k):
+#             if not(0<=i<len(board) and 0<=j<len(board[0])) or board[i][j] != word[k]:
+#                 return False
+            
+#             if k == len(word)-1:
+#                 return True
+
+#             temp, board[i][j] = board[i][j],'v'
+
+#             found = dfs(i+1,j,k+1) or dfs(i-1, j, k+1) or dfs(i, j-1, k+1) or dfs(i,j+1,k+1)
+
+#             board[i][j] = temp
+#             return found
+
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if dfs(i,j,0):
+#                     return True
+
+#         return False                 
