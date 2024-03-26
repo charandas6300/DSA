125e921012d8f24da6dfe7f5d750338343e621f1
charandas6300 <charandas.v20@iiits.in>
2024-03-27 00:01:06 +0530

    27/03

--- problems/DSA.py
+++ problems/DSA.py
@@ -655,3 +655,34 @@
 # result = maximumValue(items, 4, W)
 
 # print(result)
+########################recursive frog jump
+# def frogJump(n: int, heights: List[int]) -> int:
+#     memory = {}
+#     def frogN(ind):
+#         if ind == 0:
+#             return 0
+#         if ind in memory:
+#             return memory[ind]    
+#         left = frogN(ind-1)+abs(heights[ind-1] - heights[ind])
+#         right = float('inf')
+#         if(ind>1): 
+#             right = frogN(ind-2)+abs(heights[ind-2]-heights[ind])
+
+#         memory[ind] = min(left,right)    
+#         return memory[ind]
+
+#     return frogN(n-1) 
+############DP
+# def frogJump(n: int, heights: List[int]) -> int:
+#     if n<=1:
+#         return 0 
+
+#     dp = [0]*n
+#     dp[1] = abs(heights[1]-heights[0])
+
+#     for i in range(2,n):
+#         left = dp[i-1]+abs(heights[i-1]-heights[i])
+#         right = dp[i-2] + abs(heights[i-2]-heights[i])
+#         dp[i] = min(left,right)
+        
+#     return dp[n-1]   
